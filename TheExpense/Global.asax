51e3c9e82dafe132b3b0f748821382d497698d9e
bluerain303 <bluerain303yl@yahoo.com>
2014-06-04 22:00:58 +0200

    remaining files

---
+++ TheExpense/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheExpense.MvcApplication" Language="C#" %>
---
+++ TheExpense/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheExpense.Startup))]
+namespace TheExpense
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
